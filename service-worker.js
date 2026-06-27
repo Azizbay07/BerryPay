5aa5643749da7cf054d425f7f59814b2f9893c8b
Azizbay07 <adiletteitov43@gmail.com>
2026-06-27 16:45:16 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,1 @@
-const CACHE='berrypay-v82-pro';
-const FILES=['./','./index.html','./style.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+self.addEventListener('install',e=>self.skipWaiting());
